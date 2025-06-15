a33e7a1a16432dc9baa6b5da95dcb33f44f7fa35
doe1080 <98906116+doe1080@users.noreply.github.com>
2025-06-16 05:33:15 +0900

    [ie/reuters] Rework Extractors

--- yt_dlp/extractor/megaphone.py
+++ yt_dlp/extractor/megaphone.py
@@ -1,46 +1,94 @@
+import itertools
+
 from .common import InfoExtractor
-from ..utils import js_to_json
+from ..utils import (
+    ExtractorError,
+    clean_html,
+    float_or_none,
+    parse_iso8601,
+    parse_qs,
+    update_url,
+    url_or_none,
+)
+from ..utils.traversal import require, traverse_obj
 
 
 class MegaphoneIE(InfoExtractor):
-    IE_NAME = 'megaphone.fm'
-    IE_DESC = 'megaphone.fm embedded players'
-    _VALID_URL = r'https?://player\.megaphone\.fm/(?P<id>[A-Z0-9]+)'
-    _EMBED_REGEX = [rf'<iframe[^>]*?\ssrc=["\'](?P<url>{_VALID_URL})']
-    _TEST = {
+    IE_NAME = 'megaphone'
+
+    _BASE_URL = 'https://player.megaphone.fm'
+    _VALID_URL = r'https?://(?:play(?:er|list))\.megaphone\.fm/(?:\?[ep]=)?(?P<id>[A-Z0-9]+)'
+    _EMBED_REGEX = [rf'<iframe[^>]+\bsrc\s*=\s*["\'](?P<url>{_VALID_URL})']
+    _TESTS = [{
         'url': 'https://player.megaphone.fm/GLT9749789991',
-        'md5': '4816a0de523eb3e972dc0dda2c191f96',
         'info_dict': {
             'id': 'GLT9749789991',
             'ext': 'mp3',
             'title': '#97 What Kind Of Idiot Gets Phished?',
-            'thumbnail': r're:^https://.*\.png.*$',
+            'description': 'md5:8fc2ba1da0efb099ef928df127358a90',
             'duration': 1998.36,
-            'creators': ['Reply All'],
+            'release_date': '20170518',
+            'release_timestamp': 1495101600,
+            'series': 'Reply All',
+            'thumbnail': r're:https?://megaphone\.imgix\.net/podcasts/.+\.(?:jpe?g|png)',
         },
-    }
+    }, {
+        'url': 'https://playlist.megaphone.fm/?e=THRH8451800069',
+        'info_dict': {
+            'id': 'THRH8451800069',
+            'ext': 'mp3',
+            'title': 'Drone wars',
+            'description': 'md5:3e87ebb3f1b41d68c802398e4c5c57bd',
+            'duration': 1246.32,
+            'release_date': '20250614',
+            'release_timestamp': 1749895200,
+            'series': 'Reuters World News',
+            'thumbnail': r're:https?://megaphone\.imgix\.net/podcasts/.+\.(?:jpe?g|png)',
+        },
+    }, {
+        'url': 'https://playlist.megaphone.fm/?p=BL5764516365',
+        'info_dict': {
+            'id': 'BL5764516365',
+        },
+        'playlist_count': 65,
+    }]
+
+    def _entries(self, playlist_id):
+        for page in itertools.count(1):
+            playlist = self._download_json(
+                f'{self._BASE_URL}/playlist/{playlist_id}', playlist_id,
+                f'Downloading page {page}', query={'page': page})
+            for episode_id in traverse_obj(playlist, (
+                'episodes', ..., 'uid', {str}, filter, all, filter,
+            )):
+                yield self.url_result(f'{self._BASE_URL}/{episode_id}')
+
+            if playlist['limit'] * page >= playlist['totalCount']:
+                break
 
     def _real_extract(self, url):
-        video_id = self._match_id(url)
-        webpage = self._download_webpage(url, video_id)
+        megaphone_id = self._match_id(url)
+        query = {k: v[0] for k, v in parse_qs(url).items() if v}
+        if 'p' in query:
+            return self.playlist_result(self._entries(megaphone_id), megaphone_id)
 
-        title = self._og_search_property('audio:title', webpage)
-        author = self._og_search_property('audio:artist', webpage)
-        thumbnail = self._og_search_thumbnail(webpage)
-
-        episode_json = self._search_regex(r'(?s)var\s+episode\s*=\s*(\{.+?\});', webpage, 'episode JSON')
-        episode_data = self._parse_json(episode_json, video_id, js_to_json)
-        video_url = self._proto_relative_url(episode_data['mediaUrl'], 'https:')
-
-        formats = [{
-            'url': video_url,
-        }]
+        episode = self._download_json(
+            f'{self._BASE_URL}/playlist/episode/{megaphone_id}',
+            megaphone_id, expected_status=404)
+        if errors := episode.get('errors'):
+            raise ExtractorError(errors, expected=True)
 
         return {
-            'id': video_id,
-            'thumbnail': thumbnail,
-            'title': title,
-            'creators': [author] if author else None,
-            'duration': episode_data['duration'],
-            'formats': formats,
+            'id': megaphone_id,
+            'series': traverse_obj(episode, ('podcastTitle', {clean_html})),
+            'vcodec': 'none',
+            **traverse_obj(episode, ('episodes', ..., {
+                'title': ('title', {clean_html}),
+                'alt_title': ('subtitle', {clean_html}, filter),
+                'description': ('summary', {clean_html}),
+                'duration': ('duration', {float_or_none}),
+                'release_timestamp': ('pubDate', {parse_iso8601}),
+                'thumbnail': ('imageUrl', {url_or_none}, {update_url(query=None)}),
+                'url': ('audioUrl', {self._proto_relative_url}, {url_or_none}, {require('podcast source')}),
+            }, any)),
         }
--- yt_dlp/extractor/reuters.py
+++ yt_dlp/extractor/reuters.py
@@ -1,66 +1,101 @@
-import re
-
 from .common import InfoExtractor
 from ..utils import (
-    int_or_none,
-    js_to_json,
-    unescapeHTML,
+    clean_html,
+    float_or_none,
+    parse_iso8601,
+    str_or_none,
+    url_or_none,
 )
+from ..utils.traversal import require, traverse_obj
 
 
 class ReutersIE(InfoExtractor):
-    _WORKING = False
-    _VALID_URL = r'https?://(?:www\.)?reuters\.com/.*?\?.*?videoId=(?P<id>[0-9]+)'
-    _TEST = {
-        'url': 'http://www.reuters.com/video/2016/05/20/san-francisco-police-chief-resigns?videoId=368575562',
-        'md5': '8015113643a0b12838f160b0b81cc2ee',
+    _VALID_URL = r'https?://(?:[^/]+\.)?reuters\.com/(?P<type>[^/]+)/(?:[^/]+/)*(?P<id>[^/?#&]+)(?:[/?#]|$)'
+    _TESTS = [{
+        'url': 'https://www.reuters.com/video/watch/idRW929604062025RP1/',
         'info_dict': {
-            'id': '368575562',
+            'id': 'RW929604062025RP1',
             'ext': 'mp4',
-            'title': 'San Francisco police chief resigns',
+            'title': 'Iraqi farmers use sprinklers to grow crops in desert',
+            'description': 'md5:1f757f36e42c1fd3caf2b7703f766a57',
+            'display_id': 'idRW929604062025RP1',
+            'duration': 119.285,
+            'modified_date': '20250605',
+            'modified_timestamp': 1749135780,
+            'release_timestamp': 1749135015,
+            'release_date': '20250605',
+            'thumbnail': r're:https?://ajo\.prod\.reuters\.tv/api/v2/img/.+',
         },
-    }
+    }, {
+        'url': 'https://www.reuters.com/sustainability/sustainable-finance-reporting/uk-economy-shrinks-by-03-april-ons-says-2025-06-12/',
+        'info_dict': {
+            'id': 'RW123512062025RP1',
+            'ext': 'mp4',
+            'title': 'UK economy shrinks by the most since 2023 as U.S. tariffs hit',
+            'description': 'md5:d86475d0b5dcb8268d49c99fbce92e5c',
+            'display_id': 'uk-economy-shrinks-by-03-april-ons-says-2025-06-12',
+            'duration': 78.745,
+            'modified_date': '20250612',
+            'modified_timestamp': 1749726678,
+            'release_timestamp': 1749723417,
+            'release_date': '20250612',
+            'thumbnail': r're:https?://ajo\.prod\.reuters\.tv/api/v2/img/.+',
+        },
+    }, {
+        'url': 'https://www.reuters.com/podcasts/drone-wars-2025-06-14/',
+        'info_dict': {
+            'id': 'THRH8451800069',
+            'ext': 'mp3',
+            'title': 'Drone wars',
+            'description': 'md5:3e87ebb3f1b41d68c802398e4c5c57bd',
+            'display_id': 'THRH8451800069',
+            'duration': 1246.32,
+            'release_timestamp': 1749895200,
+            'release_date': '20250614',
+            'series': 'Reuters World News',
+            'thumbnail': r're:https?://megaphone\.imgix\.net/podcasts/.+\.(?:jpe?g|png)',
+        },
+        'add_ie': ['Megaphone'],
+    }]
 
     def _real_extract(self, url):
-        video_id = self._match_id(url)
-        webpage = self._download_webpage(
-            f'http://www.reuters.com/assets/iframe/yovideo?videoId={video_id}', video_id)
-        video_data = js_to_json(self._search_regex(
-            r'(?s)Reuters\.yovideo\.drawPlayer\(({.*?})\);',
-            webpage, 'video data'))
+        url_type, display_id = self._match_valid_url(url).group('type', 'id')
+        webpage = self._download_webpage(url, display_id)
 
-        def get_json_value(key, fatal=False):
-            return self._search_regex(rf'"{key}"\s*:\s*"([^"]+)"', video_data, key, fatal=fatal)
+        if url_type == 'podcasts':
+            cache = self._search_json(
+                r'Fusion\.contentCache\s*=', webpage, 'content cache', display_id)
+            megaphone_id = traverse_obj(cache, (
+                'transcript-by-episode-id-v1', ..., 'data', 'result',
+                'episode', 'uid', {str}, any, {require('megaphone ID')}))
+            return self.url_result(
+                f'https://player.megaphone.fm/{megaphone_id}', 'Megaphone')
 
-        title = unescapeHTML(get_json_value('title', fatal=True))
-        mmid, fid = re.search(r',/(\d+)\?f=(\d+)', get_json_value('flv', fatal=True)).groups()
+        content = self._search_json(
+            r'Fusion\.globalContent\s*=', webpage, 'global content', display_id)
+        if url_type == 'video':
+            m3u8_url = self._og_search_property('video:url', webpage, 'manifest URL')
+        else:
+            m3u8_url = traverse_obj(self._yield_json_ld(webpage, display_id), (
+                lambda _, v: v['video']['@type'] == 'VideoObject', 'video',
+                'embedUrl', {url_or_none}, any, {require('manifest URL')}))
 
-        mas_data = self._download_json(
-            f'http://mas-e.cds1.yospace.com/mas/{mmid}/{fid}?trans=json',
-            video_id, transform_source=js_to_json)
-        formats = []
-        for f in mas_data:
-            f_url = f.get('url')
-            if not f_url:
-                continue
-            method = f.get('method')
-            if method == 'hls':
-                formats.extend(self._extract_m3u8_formats(
-                    f_url, video_id, 'mp4', 'm3u8_native', m3u8_id='hls', fatal=False))
-            else:
-                container = f.get('container')
-                ext = '3gp' if method == 'mobile' else container
-                formats.append({
-                    'format_id': ext,
-                    'url': f_url,
-                    'ext': ext,
-                    'container': container if method != 'mobile' else None,
-                })
+        video = traverse_obj(content, (
+            'result', ('related_content', None), 'videos',
+            lambda _, v: v['source']['hls'] == m3u8_url, any))
+        formats, subtitles = self._extract_m3u8_formats_and_subtitles(m3u8_url, display_id, 'mp4')
 
         return {
-            'id': video_id,
-            'title': title,
-            'thumbnail': get_json_value('thumb'),
-            'duration': int_or_none(get_json_value('seconds')),
+            'display_id': display_id,
             'formats': formats,
+            'subtitles': subtitles,
+            **traverse_obj(video, {
+                'id': ('id', {str_or_none}),
+                'title': ('title', {clean_html}),
+                'duration': ('duration', {float_or_none}),
+                'description': ('description', {clean_html}),
+                'modified_timestamp': ('updated_time', {parse_iso8601}),
+                'release_timestamp': ('published_time', {parse_iso8601}),
+                'thumbnail': ('thumbnail', 'url', {url_or_none}),
+            }),
         }
