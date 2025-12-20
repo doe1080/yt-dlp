56610d1d945a86c097358bbc91750eeeba2e31cd
doe1080 <98906116+doe1080@users.noreply.github.com>
2025-12-21 07:38:56 +0900

    [ie/kick] Code cleanup, Comment support

--- yt_dlp/extractor/kick.py
+++ yt_dlp/extractor/kick.py
@@ -1,18 +1,24 @@
+import datetime as dt
 import functools
+import re
 import urllib.parse
+import xml.etree.ElementTree as ET
 
 from .common import InfoExtractor
+from ..networking.exceptions import HTTPError
 from ..utils import (
+    ExtractorError,
     UserNotLive,
+    clean_html,
     determine_ext,
     float_or_none,
     int_or_none,
     parse_iso8601,
     str_or_none,
-    traverse_obj,
     unified_timestamp,
     url_or_none,
 )
+from ..utils.traversal import find_element, traverse_obj
 
 
 class KickBaseIE(InfoExtractor):
@@ -27,6 +33,25 @@ class KickBaseIE(InfoExtractor):
         return self._download_json(
             f'https://kick.com/api/{path}', display_id, note=note,
             headers={**self._api_headers, **headers}, impersonate=True, **kwargs)
+
+    def _json_to_xml(self, messages, start_time):
+        root = ET.Element('packet')
+        for no, msg in enumerate(messages, 1):
+            message = traverse_obj(msg, ('content', {clean_html}, filter))
+            if msg['type'] == 'message' and message and not message.startswith('[emote:'):
+                created_at = parse_iso8601(msg['created_at'])
+                chat = ET.SubElement(root, 'chat', {
+                    'user': msg['sender']['slug'],
+                    'user_id': str(msg['user_id']),
+                    'no': str(no),
+                    'vpos': str((created_at - start_time) * 100),
+                    'date': str(created_at),
+                })
+                chat.text = message
+
+        xml_declaration = "<?xml version='1.0' encoding='UTF-8'?>\n"
+        xml_body = ET.tostring(root, 'utf-8').decode('utf-8').replace('><', '>\n<')
+        return xml_declaration + xml_body
 
 
 class KickIE(KickBaseIE):
@@ -93,73 +118,118 @@ class KickIE(KickBaseIE):
 
 class KickVODIE(KickBaseIE):
     IE_NAME = 'kick:vod'
-    _VALID_URL = r'https?://(?:www\.)?kick\.com/[\w-]+/videos/(?P<id>[\da-f]{8}-(?:[\da-f]{4}-){3}[\da-f]{12})'
+    _VALID_URL = r'https?://(?:www\.)?kick\.com/(?P<channel>[\w-]+)/videos/(?P<id>[\da-f]{8}-(?:[\da-f]{4}-){3}[\da-f]{12})'
     _TESTS = [{
-        # Regular VOD
-        'url': 'https://kick.com/xqc/videos/5c697a87-afce-4256-b01f-3c8fe71ef5cb',
+        'url': 'https://kick.com/xqc/videos/5a68dcd3-f248-4603-87a2-35c93f564ca6',
         'info_dict': {
-            'id': '5c697a87-afce-4256-b01f-3c8fe71ef5cb',
+            'id': '5a68dcd3-f248-4603-87a2-35c93f564ca6',
             'ext': 'mp4',
-            'title': '🐗LIVE🐗CLICK🐗HERE🐗DRAMA🐗ALL DAY🐗NEWS🐗VIDEOS🐗CLIPS🐗GAMES🐗STUFF🐗WOW🐗IM HERE🐗LETS GO🐗COOL🐗VERY NICE🐗',
-            'description': 'THE BEST AT ABSOLUTELY EVERYTHING. THE JUICER. LEADER OF THE JUICERS.',
-            'uploader': 'xQc',
-            'uploader_id': '676',
+            'title': '😫LIVE😫CLCK😫DRAMA😫NEWS😫THINGS😫VIDEOS😫CLIPS😫GAMES😫LIVE😫QUICK😫CLICK IT😫DONT MISS IT😫BEST GAMER 2025😫TOO EZ😫',
+            'age_limit': 18,
+            'categories': ['Just Chatting'],
             'channel': 'xqc',
             'channel_id': '668',
+            'duration': 45368,
+            'live_status': 'was_live',
+            'modified_date': str,
+            'modified_timestamp': int,
+            'release_date': '20251130',
+            'release_timestamp': 1764536147,
+            'thumbnail': r're:https?://.+\.webp',
+            'timestamp': 1764536145,
+            'upload_date': '20251130',
+            'uploader': 'xQc',
+            'uploader_id': '676',
             'view_count': int,
-            'age_limit': 18,
-            'duration': 22278.0,
-            'thumbnail': r're:^https?://.*\.jpg',
-            'categories': ['Deadlock'],
-            'timestamp': 1756082443,
-            'upload_date': '20250825',
         },
-        'params': {'skip_download': 'm3u8'},
-    }, {
-        # VOD of ongoing livestream (at the time of writing the test, ID rotates every two days)
-        'url': 'https://kick.com/a-log-burner/videos/5230df84-ea38-46e1-be4f-f5949ae55641',
-        'info_dict': {
-            'id': '5230df84-ea38-46e1-be4f-f5949ae55641',
-            'ext': 'mp4',
-            'title': r're:😴 Cozy Fireplace ASMR 🔥 | Relax, Focus, Sleep 💤',
-            'description': 'md5:080bc713eac0321a7b376a1b53816d1b',
-            'uploader': 'A_Log_Burner',
-            'uploader_id': '65114691',
-            'channel': 'a-log-burner',
-            'channel_id': '63967687',
-            'view_count': int,
-            'age_limit': 18,
-            'thumbnail': r're:^https?://.*\.jpg',
-            'categories': ['Other, Watch Party'],
-            'timestamp': int,
-            'upload_date': str,
-            'live_status': 'is_live',
-        },
-        'skip': 'live',
+        'expected_warnings': ['The extractor is attempting impersonation'],
     }]
 
     def _real_extract(self, url):
-        video_id = self._match_id(url)
-        response = self._call_api(f'v1/video/{video_id}', video_id)
+        channel, video_id = self._match_valid_url(url).group('channel', 'id')
+        webpage = self._download_webpage(url, video_id)
+        video = traverse_obj(self._call_api(
+            f'v2/channels/{channel}/videos', channel,
+        ), (lambda _, v: v['video']['uuid'] == video_id, any))
 
         return {
             'id': video_id,
-            'formats': self._extract_m3u8_formats(response['source'], video_id, 'mp4'),
-            **traverse_obj(response, {
-                'title': ('livestream', ('session_title', 'slug'), {str}, any),
-                'description': ('livestream', 'channel', 'user', 'bio', {str}),
-                'channel': ('livestream', 'channel', 'slug', {str}),
-                'channel_id': ('livestream', 'channel', 'id', {int}, {str_or_none}),
-                'uploader': ('livestream', 'channel', 'user', 'username', {str}),
-                'uploader_id': ('livestream', 'channel', 'user_id', {int}, {str_or_none}),
-                'timestamp': ('created_at', {parse_iso8601}),
-                'duration': ('livestream', 'duration', {float_or_none(scale=1000)}),
-                'thumbnail': ('livestream', 'thumbnail', {url_or_none}),
-                'categories': ('livestream', 'categories', ..., 'name', {str}),
-                'view_count': ('views', {int_or_none}),
-                'age_limit': ('livestream', 'is_mature', {bool}, {lambda x: 18 if x else 0}),
-                'is_live': ('livestream', 'is_live', {bool}),
+            'channel': channel,
+            'formats': self._extract_m3u8_formats(video['source'], video_id, 'mp4'),
+            'subtitles': self.extract_subtitles(video),
+            'uploader': traverse_obj(webpage, ({find_element(id='channel-username')}, {clean_html}, filter)),
+            **traverse_obj(video, {
+                'title': ('session_title', {clean_html}),
+                'age_limit': ('is_mature', {bool}, {lambda x: 18 if x else None}),
+                'categories': ('categories', ..., 'name', {str}, filter),
+                'duration': ('duration', {int_or_none(scale=1000)}),
+                'is_live': ('is_live', {bool}),
+                'live_status': ('is_live', {bool}, {lambda x: 'is_live' if x else 'was_live'}),
+                'tags': ('tags', ..., {str}, filter),
+                'thumbnails': ('thumbnail', 'srcset', {lambda x: [{
+                    'height': int_or_none(height),
+                    'url': url_or_none(url),
+                    'width': int_or_none(width),
+                } for url, height, width in re.findall(
+                    r'(https?://[^,\s]+/(\d+)\.webp)\s+(\d+)w', x,
+                )]}),
+                'timestamp': ('start_time', {unified_timestamp}),
             }),
+            **traverse_obj(video, ('channel', {
+                'channel_id': ('id', {str_or_none}),
+                'uploader_id': ('user_id', {str_or_none}),
+            })),
+            **traverse_obj(video, ('video', {
+                'modified_timestamp': ('updated_at', {parse_iso8601}),
+                'release_timestamp': ('created_at', {parse_iso8601}),
+                'view_count': ('views', {int_or_none}),
+            })),
+        }
+
+    def _get_subtitles(self, resp):
+        channel_id = resp['channel_id']
+        start_time = current_time = unified_timestamp(resp['start_time'])
+        duration = float_or_none(resp['duration'], 1000)
+        end_time = start_time + duration
+
+        seen_ids, msgs = set(), []
+        while current_time <= end_time:
+            next_pos = current_time + 5
+            query = dt.datetime.fromtimestamp(current_time, dt.timezone.utc).isoformat()
+            try:
+                pct = float_or_none(current_time - start_time, invscale=100, scale=duration, default=0) if duration else 0
+                history = self._download_json(
+                    f'https://web.kick.com/api/v1/chat/{channel_id}/history', channel_id,
+                    f'Downloading {pct:.2f}% chat data', query={'start_time': query})['data']
+                for msg in traverse_obj(history, (
+                    'messages', lambda _, v: v['type'] == 'message',
+                )):
+                    msg_id = traverse_obj(msg, ('id', {str_or_none}))
+                    if msg_id and msg_id not in seen_ids:
+                        seen_ids.add(msg_id)
+                        msgs.append(msg)
+
+                if cursor := traverse_obj(history, (
+                    'cursor', {float_or_none(scale=1_000_000)},
+                )):
+                    if cursor > current_time:
+                        next_pos = cursor
+                elif created_at := traverse_obj(history, (
+                    'messages', ..., 'created_at', {parse_iso8601}, {max},
+                )):
+                    next_pos = max(next_pos, created_at + 5)
+                current_time = next_pos
+            except ExtractorError as e:
+                if isinstance(e.cause, HTTPError) and e.cause.status == 400:
+                    raise ExtractorError(self._parse_json(
+                        e.cause.response.read().decode(), channel_id)['message'], expected=True)
+        msgs.sort(key=lambda x: parse_iso8601(x['created_at']))
+
+        return {
+            'danmaku': [{
+                'ext': 'xml',
+                'data': self._json_to_xml(msgs, start_time),
+            }],
         }
 
 
